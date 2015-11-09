c06e1aee5a6197cfd2f9cc85c34a2875d8dad95f
Chris Smith <smitty1e@gmail.com>
2015-11-09 14:38:09 -0500

    added layout

--- pywordup/pywordup.py
+++ pywordup/pywordup.py
@@ -20,3 +20,25 @@ class PuzzleLayout(object):
     def __init__(self, puzzle_source, puzzle_width):
         self.puzzle_source = puzzle_source
         self.puzzle_width  = puzzle_width
+        self.puzzle_lines  = []
+        self.current_line  = ""
+        
+    def do_layout(self):
+        """Map the PuzzleSource into the upper half of the puzzle.
+        """
+        def do_word( some_word ):
+            """Try to add SOMEO_WORD to the current line.
+
+               If word no fit-fit, add current_line to puzzle_lines. 
+            """
+            if (len(some_word) + len(self.current_line) + 1 > self.puzzle_width):
+                self.puzzle_lines.append( self.current_line )
+                self.current_line  = " "
+                self.current_line  = some_word + " "
+            else:
+                self.current_line += some_word + " "
+
+                
+        [do_word( w ) for w in self.puzzle_source.next_word()]
+        self.puzzle_lines.append( self.current_line )
+        return self.puzzle_lines
--- tests/test_pywordup.py
+++ tests/test_pywordup.py
@@ -18,9 +18,13 @@ class TestPywordup(unittest.TestCase):
 
     def setUp(self):
         self.p = pywordup.PuzzleSource(TEST_STRING, "Herb Sutter")
+        self.l = pywordup.PuzzleLayout( self.p, 25 )
         
     def test_next_word(self):
         [print(w) for w in self.p.next_word()]
+
+    def test_do_layout(self):
+        print(self.l.do_layout())
 
     def tearDown(self):
         pass
