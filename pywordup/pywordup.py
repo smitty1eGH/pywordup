1c13449d092434f9243762d65bc18c757cf069fa
unknown <christosmith@USARRCHRISTOSM3.us.deloitte.com>
2015-11-09 19:24:00 -0500

    Format class now passes test.

--- pywordup/pywordup.py
+++ pywordup/pywordup.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+
+from random import shuffle
+
 class PuzzleSource(object):
     """PuzzleSource receives a chunk of text and attribution for a puzzle
        
@@ -26,19 +29,67 @@ class PuzzleLayout(object):
     def do_layout(self):
         """Map the PuzzleSource into the upper half of the puzzle.
         """
+        def pad_space(line_len):
+            return " " * (self.puzzle_width - line_len)
+
         def do_word( some_word ):
             """Try to add SOMEO_WORD to the current line.
 
                If word no fit-fit, add current_line to puzzle_lines. 
             """
             if (len(some_word) + len(self.current_line) + 1 > self.puzzle_width):
-                self.puzzle_lines.append( self.current_line )
-                self.current_line  = " "
+                line_len = len(self.current_line)
+                self.puzzle_lines.append( self.current_line + pad_space(line_len))
+                self.current_line  = ""
                 self.current_line  = some_word + " "
             else:
                 self.current_line += some_word + " "
 
-                
+        #Go through the input words and write them out to a list of lines.
+        #  Afterward, handle the leftover words, and he attribution.
         [do_word( w ) for w in self.puzzle_source.next_word()]
-        self.puzzle_lines.append( self.current_line )
+        self.puzzle_lines.append( self.current_line         + pad_space( len(self.current_line)))
+        self.puzzle_lines.append( self.puzzle_source.attrib + pad_space( len(self.current_line)))
+        
         return self.puzzle_lines
+
+class PuzzleFormat(object):
+    """PuzzleFormat receives the list of strings and clue lines 
+       That will become the puzzle when fed to the publisher
+    """
+    def __init__(self, puzzle_lines, puzzle_width):
+        self.line_count   = len(puzzle_lines)
+        self.puzzle_lines = puzzle_lines
+        self.puzzle_width = puzzle_width
+        self.vertical     = []                      #will hold a vertical slice of puzzle_lines
+        self.puzzle_lines = puzzle_lines
+        self.clue_lines   = [ "" ] #holds munged values of self.vertical
+        [self.clue_lines.append("") for x in range(0, self.line_count-1)]
+        
+    def do_format(self):
+        """
+        """
+        def vertical_slice(j):
+            """Take the vertical slice of the input character strings
+               at j. Omit spaces. Append to self.vertical
+            """
+            x = 0
+            self.vertical = [""]
+            for x in range(0, self.line_count-1):
+                y = self.puzzle_lines[x][j:j+1]
+                if x != " ":
+                    self.vertical.append( y )
+                else:
+                    print("%s\t%s" % (x,y))
+            return shuffle( self.vertical )
+        
+        j = 0
+        k = 0
+
+        for j in range(0, self.puzzle_width -1):
+            vertical_slice( j )
+            #print(self.vertical)
+            for k in range(0, len(self.vertical) -1):
+                self.clue_lines[k] += self.vertical[k]
+
+
--- tests/test_pywordup.py
+++ tests/test_pywordup.py
@@ -13,12 +13,14 @@ import unittest
 from pywordup import pywordup
 
 TEST_STRING = "The difference between theory and practice is greater in practice than in theory."
+TEST_WIDTH  = 25
 
 class TestPywordup(unittest.TestCase):
 
     def setUp(self):
-        self.p = pywordup.PuzzleSource(TEST_STRING, "Herb Sutter")
-        self.l = pywordup.PuzzleLayout( self.p, 25 )
+        self.p = pywordup.PuzzleSource( TEST_STRING       ,"Herb Sutter" )
+        self.l = pywordup.PuzzleLayout( self.p            , TEST_WIDTH )
+        self.f = pywordup.PuzzleFormat( self.l.do_layout(), TEST_WIDTH )
         
     def test_next_word(self):
         [print(w) for w in self.p.next_word()]
@@ -26,6 +28,11 @@ class TestPywordup(unittest.TestCase):
     def test_do_layout(self):
         print(self.l.do_layout())
 
+    def test_puzzle_format(self):
+        self.f.do_format()
+        print(self.f.puzzle_lines)
+        print(self.f.clue_lines)
+        
     def tearDown(self):
         pass
 
