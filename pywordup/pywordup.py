dbdab286a7834c9f1908f896535bcb98ddee66c7
unknown <christosmith@USARRCHRISTOSM3.us.deloitte.com>
2015-11-17 19:52:59 -0500

    First actual usage

--- pywordup/pywordup.py
+++ pywordup/pywordup.py
@@ -6,7 +6,7 @@ def puzzleSource(chunk):
     """Generator for words of a puzzle
     """
     for w in chunk.split(" "):
-        yield w
+        yield w + " "
 
 class PuzzleLayout(object):
     """Write the PuzzleSource out as a puzzle.
@@ -31,10 +31,9 @@ class PuzzleLayout(object):
             line_len = len(self.current_line)
             if ((len(some_word) + line_len) > self.puzzle_width):
                 self.puzzle_lines.append(self.current_line + pad_space(line_len))
-                self.current_line = ""
-                self.current_line = some_word + " "
+                self.current_line = some_word
             else:
-                self.current_line += some_word + " "
+                self.current_line += some_word
 
         #Go through the input words and write them out to a list of lines.
         #  Afterward, handle the leftover words, and he attribution.
@@ -52,8 +51,9 @@ class PuzzleFormat(object):
         self.line_count   = len(puzzle_lines)
         self.puzzle_lines = puzzle_lines
         self.puzzle_width = puzzle_width
-        self.vertical     = []            #will hold a vertical slice of puzzle_lines
+        self.vertical     = []            #will hold a vertical slice of puxzzle_lines
         self.puzzle_lines = puzzle_lines
+        self.divider      = 'x' * puzzle_width
         self.clue_lines   = []            #list of lists of  munged values of self.vertical
         [self.clue_lines.append([]) for x in range(0, self.line_count)]
         
@@ -65,13 +65,13 @@ class PuzzleFormat(object):
                at j. Omit spaces. Append to self.vertical
             """
             self.vertical = [""]
-            [self.vertical.append(self.puzzle_lines[x][j:j+1])
+            [self.vertical.append((self.puzzle_lines[x][j:j+1]).lower())
                 for x in range(0, self.line_count)]
             #return shuffle(self.vertical)
             return self.vertical.sort(reverse=True)
         for j in range(0, self.puzzle_width):
             vertical_slice(j)
-            print(self.vertical)
+            #print(self.vertical)
             for k in range(0,self.line_count):
                 self.clue_lines[k].append(self.vertical[k])
             
@@ -79,13 +79,35 @@ def puzzlePublish(puzzle_format):
     """PuzzlePublish takes the PuzzleFormat instance and renders its 
        puzzle_lines and clue_lines as HTML
     """
+    FULL_BLOCK = "&#9608;"
+    HALF_BLOCK = "&#9600;"    
     #make a big list of all of the characters as lists of TD tags
-    rows = [["<td>%s</td>" % c for c in x]
-                               for x in puzzle_format.puzzle_lines] \
+    puzz = [["<td>%s</td>" % "&nbsp;"   if   c != ' ' \
+                                        else          \
+             "<td>%s</td>" % FULL_BLOCK for  c in x]  \
+                                        for  x in puzzle_format.puzzle_lines] \
+         + [["<td>%s</td>" % HALF_BLOCK for c in puzzle_format.divider]] \
+         + [["<td>%s</td>" % c for c in x]
+                               for x in puzzle_format.clue_lines]
+
+    answ = [["<td>%s</td>" % c          if   c != ' ' \
+                                        else          \
+             "<td>%s</td>" % FULL_BLOCK for  c in x]  \
+                                        for  x in puzzle_format.puzzle_lines] \
+         + [["<td>%s</td>" % HALF_BLOCK for c in puzzle_format.divider]] \
          + [["<td>%s</td>" % c for c in x]
                                for x in puzzle_format.clue_lines]
 
     #make a TABLE
     return "<table border='1'>"    \
-         + "".join(["<tr>%s</tr>"  % "".join(r) for r in rows]) \
+         + "".join(["<tr>%s</tr>"  % "".join(r) for r in puzz]) \
+         + "</table><br/><br/>"    \
+         + "<table border='1'>"    \
+         + "".join(["<tr>%s</tr>"  % "".join(r) for r in answ]) \
          + "</table>"
+
+
+
+
+
+
--- tests/test_pywordup.py
+++ tests/test_pywordup.py
@@ -18,12 +18,17 @@ TEST_WIDTH  = 25
 TEST_STR2   = "hop head red"
 TEST_WID2   = 4
 
+TEST_VS0    = "And so was also James, and John, the sons of Zebedee, which were partners with Simon. And Jesus said unto Simon, Fear not; from henceforth thou shalt catch men. And when they had brought their ships to land, they forsook all, and followed him. LUKE 5:10-11"
+TEST_WI0    = 60
+
+TEST_VS1    = "And, behold, men brought in a bed a man which was taken with a palsy: and they sought means to bring him in, and to lay him before him. And when they could not find by what way they might bring him in because of the multitude, they went upon the housetop, and let him down through the tiling with his couch into the midst before Jesus. And when he saw their faith, he said unto him, Man, thy sins are forgiven thee. LUKE 5:18-20"
+
 class TestPywordup(unittest.TestCase):
 
     def setUp(self):
-        self.p = pywordup.puzzleSource( TEST_STRING )
-        self.l = pywordup.PuzzleLayout( self.p            , TEST_WIDTH )
-        self.f = pywordup.PuzzleFormat( self.l.do_layout(), TEST_WIDTH )
+        self.p = pywordup.puzzleSource( TEST_VS1 )
+        self.l = pywordup.PuzzleLayout( self.p            , TEST_WI0 )
+        self.f = pywordup.PuzzleFormat( self.l.do_layout(), TEST_WI0 )
         
     def test_next_word(self):
         [print(w) for w in self.p]
